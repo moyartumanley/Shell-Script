1ec632e2a97b7c4156d47ed4c5c8b823659c7dc3
Jonah Zimmer <jszimmer545@gmail.com>
2025-04-20 03:55:08 +0000

    task 2 complete

--- shell.c
+++ shell.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
 
 int main(void) {
     char userInput[1024];
-    char running = 1;
     char *ret;
+    int pid;
+    int status;
 
-    while (running) {
+    while (1) {
         printf("Enter a command: \n");
         ret = fgets(userInput, 1024, stdin);
-        printf("You typed: %s", userInput);
+        if (strlen(userInput) > 2) {
+            userInput[strlen(userInput) - 1] = '\0'; // Googled how to remove last two characters from string, used gemini response
+        }
         if (ret == NULL) {
-            running = 0;
+            break;
+        }
+        else {
+            pid = fork();
+            if (pid == 0) {
+                char *inputs[] = {userInput, NULL};
+                execvp(inputs[0], inputs);
+                if (strlen(userInput) > 2) {
+                    printf("execvp returned with errno: %d\n", errno);
+                }
+            }
+            else {
+                wait(&status);
+            }
         }
     }
     return 0;
